c122116c85a21ea572bb2114a598f6820000096a
Ray Lanfranco <rogue.ux@gmail.com>
2021-04-08 17:07:51 -0500

    Standard Heroku API set up for consumption by frontend.

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f55b253c37b28edc767ac13bd0a812aa'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2f72f292-92d3-42a4-b9e2-dce1143610a2'
+};
